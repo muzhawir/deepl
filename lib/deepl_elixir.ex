6746bb90a02753054a2c1c1b50676c3feaa08d53
muzhawir <muzhawir.amri@gmail.com>
2025-07-18 20:19:04 +0800

    Initial Commit

---
+++ lib/deepl_elixir.ex
@@ -0,0 +1,18 @@
+defmodule DeeplElixir do
+  @moduledoc """
+  Documentation for `DeeplElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DeeplElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
