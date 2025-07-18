97d9d78df40b49b1503867acdaaf8bd9734fff36
muzhawir <muzhawir.amri@gmail.com>
2025-07-18 23:04:40 +0800

    Add config file

---
+++ config/config.exs
@@ -0,0 +1,7 @@
+import Config
+
+# You need an authentication key to access the API.
+# You can find your key in the “API Keys” tab in your account.
+# (https://www.deepl.com/en/your-account/keys)
+
+config :deepl, api_key: nil
