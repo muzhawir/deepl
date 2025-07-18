3278850f11df1b74a9d09a99f3f1893ea47fa52d
muzhawir <muzhawir.amri@gmail.com>
2025-07-18 20:38:38 +0800

    Add development packages

--- mix.exs
+++ mix.exs
@@ -7,7 +7,23 @@ defmodule DeeplElixir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "deepl_elixir",
+      source_url: "https://github.com/muzhawir/deepl_elixir",
+      homepage_url: "https://hex.pm/packages/deepl_elixir"
+    ]
+  end
+
+  defp description do
+    "Elixir package for the DeepL language translation API."
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/muzhawir/deepl_elixir"}
     ]
   end
 
@@ -21,8 +37,10 @@ defmodule DeeplElixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
+      {:styler, "~> 1.2", only: [:dev, :test], runtime: false}
     ]
   end
 end
