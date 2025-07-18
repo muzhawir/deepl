0ee0e66e3dad735ba204844d2a1573396627a102
muzhawir <muzhawir.amri@gmail.com>
2025-07-19 00:40:21 +0800

    Add config

--- config/config.exs
+++ config/config.exs
@@ -4,4 +4,6 @@ import Config
 # You can find your key in the “API Keys” tab in your account.
 # (https://www.deepl.com/en/your-account/keys)
 
-config :deepl, api_key: nil
+if Mix.env() in [:dev, :test] do
+  config :deepl, api_key: "c5bfe33a-0487-4e37-add3:fx"
+end
