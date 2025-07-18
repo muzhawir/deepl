0aad2dfbadafcebf221dbfcf08fd23a72c01a233
muzhawir <muzhawir.amri@gmail.com>
2025-07-19 00:41:08 +0800

    Add Deepl.Usage module

---
+++ lib/deepl/usage.ex
@@ -0,0 +1,41 @@
+defmodule Deepl.Usage do
+  @moduledoc """
+  Module for retrieving usage and quota for the current billing period with the DeepL API.
+
+  For more information, refer to the retrieve usage and quota
+  [documentation](https://developers.deepl.com/api-reference/usage-and-quota).
+  """
+
+  alias Req.Request
+
+  @base_url Deepl.base_url!() <> "/usage"
+
+  @doc """
+  Retrieve current usage and quota information.
+
+  ## Examples
+
+      iex> Deepl.Usage.get()
+      %{
+        "character_count": 180118,
+        "character_limit": 1250000
+      }
+
+  """
+  @spec get() :: map()
+  def get do
+    {_request, response} =
+      [
+        method: :get,
+        url: @base_url,
+        headers: [
+          {"Accept", "application/json"},
+          {"Authorization", "DeepL-Auth-Key " <> Deepl.get_api_key()}
+        ]
+      ]
+      |> Request.new()
+      |> Request.run_request()
+
+    JSON.decode!(response.body)
+  end
+end
