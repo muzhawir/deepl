9e6f7e35b583284373c1c70459870e4754981506
muzhawir <muzhawir.amri@gmail.com>
2025-07-19 20:19:29 +0800

    Remove runtime config

--- config/runtime.exs
+++
@@ -1,5 +0,0 @@
-import Config
-
-if config_env() == :prod do
-  config :deepl, api_key: System.fetch_env!("DEEPL_API_KEY")
-end
--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,3 @@
 import Config
 
-# You need an authentication key to access the API.
-# You can find your key in the “API Keys” tab in your account.
-# (https://www.deepl.com/en/your-account/keys)
-
-if Mix.env() in [:dev, :test] do
-  config :deepl, api_key: "test:fx"
-  config :deepl, request_behaviour: Deepl.MockRequest
-end
+config :deepl, request_behaviour: Deepl.MockRequest
