a1855f865692e1b89e7cc7458bc2a77b7a7b8e17
muzhawir <muzhawir.amri@gmail.com>
2025-07-19 20:31:27 +0800

    Implement HTTP Request mock

---
+++ lib/deepl/request_behaviour.ex
@@ -0,0 +1,11 @@
+defmodule Deepl.Request do
+  @moduledoc false
+
+  @callback run_request(term()) :: {term(), Req.Response.t() | Exception.t()}
+
+  def run_request(request), do: implement().run_request(request)
+
+  defp implement do
+    Application.get_env(:deepl, :request_behaviour, Req.Request)
+  end
+end
