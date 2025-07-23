46caeda8cc68f99c6b53763e609023c48c2a3683
muzhawir <muzhawir.amri@gmail.com>
2025-07-23 22:55:25 +0800

    Fix test.exs config

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,4 @@
 import Config
 
-config :deepl, request_behaviour: Deepl.MockRequest
-
 config :deepl, :api_key, "wwwwwwww-xxxx-yyyy-zzzz-123456789012:fx"
+config :deepl, request_behaviour: Deepl.MockRequest
