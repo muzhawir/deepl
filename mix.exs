cf18a30fa72febe5a6de2aa2087cd487395f5821
Muzhawir Amri <muzhawir.amri@gmail.com>
2025-07-23 23:11:49 +0800

    Update patch version (#5)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Deepl.MixProject do
   def project do
     [
       app: :deepl,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       description: description(),
